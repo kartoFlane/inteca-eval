0851e236b9f45a4bfe1cd12ee2c0c79b2a919a43
kartoFlane <tbachminski@gmail.com>
2018-08-07 02:30:08 +0200

    Renamed main Application class

--- src/test/java/com/kartoflane/inteca/eval/spring/ApplicationTests.java
+++ src/test/java/com/kartoflane/inteca/eval/spring/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kartoflane.springmaventest;
+package com.kartoflane.inteca.eval.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringMavenTestApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
